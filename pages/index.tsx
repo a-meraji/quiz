86b1677af8aa1fcdd50aadea2d99dd68075e891d
amin <merajiamin1997@gmail.com>
2022-04-23 01:45:42 +0430

    home page links

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,15 +17,14 @@ const Home: NextPage = () => {
           Welcome to <span className="text-yellow-400">Simple Quiz App</span>
         </h1>
 
-        <div className="mt-3 text-2xl">
+        <div className="mt-5 text-2xl">
           Get started by{' '}
           <Link href="/exam/626116a3418c1a402bd69969">
             <a className="rounded-md bg-gray-100 p-3 font-mono text-lg">
               take a text
             </a>
-          </Link>
-            {' '}
-            /{' '}
+          </Link>{' '}
+          /{' '}
           <Link href="/examiner/new-exam">
             <a className="rounded-md bg-gray-100 p-3 font-mono text-lg">
               give a text
@@ -38,28 +37,31 @@ const Home: NextPage = () => {
             <a className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-yellow-400 focus:text-yellow-400">
               <h3 className="text-2xl font-bold">Take a test &rarr;</h3>
               <p className="mt-4 text-xl">
-                Get started by Take a quick text and see how it works.
+                Get started by Take a quick text and see how it works!
+              </p>
+            </a>
+          </Link>
+
+          <Link href="/examiner/new-exam">
+            <a className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-yellow-400 focus:text-yellow-400">
+              <h3 className="text-2xl font-bold">
+                Create and save a test&rarr;
+              </h3>
+              <p className="mt-4 text-xl">
+                Create a test and save it on the database! get the id of your
+                test and share it with your examinees!
               </p>
             </a>
           </Link>
 
           <a
-            href="https://nextjs.org/learn"
+            href="https://aminblog.vercel.app/"
+            target="_blank"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-yellow-400 focus:text-yellow-400"
           >
-            <h3 className="text-2xl font-bold">Signup / Signin &rarr;</h3>
+            <h3 className="text-2xl font-bold">About Me &rarr;</h3>
             <p className="mt-4 text-xl">
-              Become a member and get access to all the features.
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-yellow-400 focus:text-yellow-400"
-          >
-            <h3 className="text-2xl font-bold">About Us &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Read more about who we are and what we do.
+              Read more about who I am and what I do.
             </p>
           </a>
 
@@ -67,8 +69,8 @@ const Home: NextPage = () => {
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-yellow-400 focus:text-yellow-400"
           >
-            <h3 className="text-2xl font-bold">Contact Us &rarr;</h3>
-            <p className="mt-4 text-xl">We are here to help you.</p>
+            <h3 className="text-2xl font-bold">Contact Me &rarr;</h3>
+            <p className="mt-4 text-xl">I would love to hear your comments and inputs!</p>
           </a>
         </div>
       </main>
